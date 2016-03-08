196e362cee8e2a9c5e3e336366bee5196c949509
suxindao <suxindao@126.com>
2016-03-08 10:45:30 +0800

    modified UI

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,17 +3,17 @@
 
 // Load the module dependencies
 var config = require('./config'),
-    mongoose = require('mongoose');
+  mongoose = require('mongoose');
 
 // Define the Mongoose configuration method
 module.exports = function() {
-    // Use Mongoose to connect to MongoDB
-    var db = mongoose.connect(config.db);
+  // Use Mongoose to connect to MongoDB
+  var db = mongoose.connect(config.db);
 
-    // Load the application models 
-    require('../app/models/user.server.model');
-    require('../app/models/article.server.model');
+  // Load the application models 
+  require('../app/models/user.server.model');
+  require('../app/models/article.server.model');
 
-    // Return the Mongoose connection instance
-    return db;
+  // Return the Mongoose connection instance
+  return db;
 };
